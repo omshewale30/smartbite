7d6a7975e0f5c127046798d0b467b67e1ebfdd3a
Om Shewale <82550011+omshewale30@users.noreply.github.com>
2025-03-06 20:53:45 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-// const API_URL = "https://smartbite-backend.vercel.app/";
-const API_URL = "http://localhost:5000/";
+const API_URL = "https://smartbite-backend.vercel.app/";
 
 export const fetchRecipes = async (data, isImage = false) => {
     try {
